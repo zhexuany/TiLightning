49ad28883da41c5abbdb9818a22965185cd37975
Zhexuan Yang <yangzhexuan@pingcap.com>
2019-10-26 08:59:52 +0800

    adding new import client

---
+++ tikv-client/src/main/java/com/pingcap/tikv/RestoreController.java
@@ -0,0 +1,7 @@
+package com.pingcap.tikv;
+
+import org.tikv.kvproto.*;
+import org.tikv.kvproto.ImportSstpb.SwitchMode;
+
+public class RestoreController {
+}
---
+++ tikv-client/src/main/java/com/pingcap/tikv/meta/StoresInfo.java
@@ -0,0 +1,19 @@
+package com.pingcap.tikv.meta;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StoresInfo {
+	private final List<StoreInfo> storeAddrs;
+
+	public StoresInfo(
+		@JsonProperty("store") List<StoreInfo> storeAddrs) {
+		this.storeAddrs = storeAddrs;
+	}
+
+	public List<StoreInfo> getStoreAddrs() {
+		return storeAddrs;
+	}
+}
